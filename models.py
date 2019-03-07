b6595a426dbec09c883b89d61c53b143e4c37190
David Bogaerts <dbogaerts@gmail.com>
2019-03-07 14:05:46 -0600

    changes to models

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
+import os
+import datetime
+from peewee import *
+from playhouse.db_url import connect
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+
+DATABASE = SqliteDatabase('crowdtalesdatabase.sqlite')
+
 class User(UserMixin, Model):
   username      = CharField(unique=True)
   email         = CharField(unique=True)
@@ -6,6 +15,25 @@ class User(UserMixin, Model):
   memberships   = ForeignKeyField(Membership, backref='membership')
   bookmarks     = ForeignKeyField(Bookmarks, backref='bookmarks')
 
+  class Meta:
+    database = DATABASE
+
+  @classmethod
+  def create_user(cls, username, email, password):
+    #sanitize email address
+    email = email.lower()
+    
+    try: #see if user exists by querying email
+      cls.select().where(cls.email == email).get() 
+    
+    except cls.DoesNotExist: #if user doesn't exist, create user
+      user = cls(username=username, email=email)
+      user.password = generate_password_hash(password)
+      user.save()
+      return user
+
+    else: #if user does exist already
+      raise Exception('Email address already in use.')
 
 class Story(Model):
   username:       = ForeignKeyField(User, backref='user')
@@ -19,6 +47,9 @@ class Story(Model):
   currentContrib  = CharField() #current person with duty to contribute
   content         = ForeignKeyField(Content, backref='content')
  
+  class Meta:
+    database = DATABASE
+
 class Content(Model):
   username  = ForeignKeyField(User, backref='user') 
   date      = DateTimeField(default=datetime.datetime.now)
@@ -27,22 +58,37 @@ class Content(Model):
   comments  = ForeignKeyField(Comment, backref='comment')
   votedate  = DateTimeField() 
 
+  class Meta:
+    database = DATABASE
+
 class Membership(Model): # this model tracks stories where user has contributed
   username  = ForeignKeyField(User, backref='user')
   story     = ForeignKeyField(Story, backref='story')
+
+  class Meta:
+    database = DATABASE
 
 class StoryQueue(Model): # this model tracks stories where user is queued to contribute
   username  = ForeignKeyField(User, backref='user')
   story     = ForeignKeyField(Story, backref='story')
 
+  class Meta:
+    database = DATABASE
+
 class Bookmark(Model): #this model used to track stories that user wants to follow, but not contribute
   username  = ForeignKeyField(User, backref='user')
   story     = ForeignKeyField(Story, backref='story')
+
+  class Meta:
+    database = DATABASE
 
 class Vote(Model):
   username  = ForeignKeyField(User, backref='user')
   vote      = CharField()
   content   = ForeignKeyField(Content, backref='content')
+
+  class Meta:
+    database = DATABASE
 
 class Comment(Model): 
   username  = ForeignKeyField(User, backref='user')
@@ -50,6 +96,11 @@ class Comment(Model):
   text      = TextField()
   comments  = ForeignKeyField('self')
 
+  class Meta:
+    database = DATABASE
 
-
+def initialize():
+  DATABASE.connect()
+  DATABASE.create_tables([User, Story, Content, Membership, StoryQueue, Bookmark, Vote, Comment], safe=True)
+  DATABASE.close()
 
