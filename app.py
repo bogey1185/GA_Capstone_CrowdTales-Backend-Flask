d564d0d2b2dffde603f2459ec1b75bc656c6d6c2
David Bogaerts <dbogaerts@gmail.com>
2019-03-08 15:23:18 -0600

    all storyqueue crud routes working

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, g, flash, redirect, url_for
 from resources.users import users_api
 from resources.stories import stories_api
-from resources.memberships import memberships_api                                                                                        
+from resources.memberships import memberships_api   
+from resources.storyqueues import storyqueues_api                                                                                       
 import config
 import models
 from flask_cors import CORS
@@ -28,9 +29,11 @@ def load_user(userid):
 CORS(users_api, origins=["http://localhost:3000"], supports_credentials=True)
 CORS(stories_api, origins=["http://localhost:3000"], supports_credentials=True)
 CORS(memberships_api, origins=["http://localhost:3000"], supports_credentials=True)
+CORS(storyqueues_api, origins=["http://localhost:3000"], supports_credentials=True)
 app.register_blueprint(users_api, url_prefix='/api/v1')
 app.register_blueprint(stories_api, url_prefix='/api/v1')
 app.register_blueprint(memberships_api, url_prefix='/api/v1')
+app.register_blueprint(storyqueues_api, url_prefix='/api/v1')
 
 @app.route('/')
 def hello_world():
---
+++ resources/storyqueues.py
@@ -0,0 +1,112 @@
+from flask import jsonify, Blueprint, abort, make_response
+from flask_restful import Resource, Api, reqparse, fields, marshal, marshal_with, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from flask_bcrypt import check_password_hash
+import json
+import models
+from flask_cors import CORS
+
+#define user response fields
+
+storyqueue_fields = {
+  'id': fields.String,
+  'user_id': fields.String,
+  'story_id': fields.String
+}
+
+class StoryQueueList(Resource):
+  def __init__(self):
+    #initialize parser
+    self.reqparse = reqparse.RequestParser()
+    #add form of args to control requests
+    self.reqparse.add_argument(
+      'user_id',
+      required=True,
+      help='No user id provided.', 
+      location=['form', 'json']
+    )
+    self.reqparse.add_argument(
+      'story_id',
+      required=True,
+      help='No story id provided.', 
+      location=['form', 'json']
+    )
+    super().__init__
+
+  #get all storyqueues
+  def get(self):
+    storyqueues = [marshal(storyqueue, storyqueue_fields) for storyqueue in models.StoryQueue.select()]
+    return {'storyqueues': storyqueues}
+
+  #create new storyqueue
+  @marshal_with(storyqueue_fields)
+  def post(self):
+    args = self.reqparse.parse_args()
+    new_storyqueue = models.StoryQueue.create(**args)
+    return new_storyqueue
+
+class StoryQueue(Resource):
+  def __init__(self):
+    #initialize parser
+    self.reqparse = reqparse.RequestParser()
+    #add form of args to control requests
+    self.reqparse.add_argument(
+      'user_id',
+      required=True,
+      help='No user id provided.', 
+      location=['form', 'json']
+    )
+    self.reqparse.add_argument(
+      'story_id',
+      required=True,
+      help='No story id provided.', 
+      location=['form', 'json']
+    )
+    super().__init__
+
+  #get specific storyqueue
+  @marshal_with(storyqueue_fields)
+  def get(self, id):
+    storyqueue = models.StoryQueue.get(models.StoryQueue.id == id)
+    return storyqueue
+
+  # edit story
+  @marshal_with(storyqueue_fields)
+  def put(self, id):
+    args = self.reqparse.parse_args()
+    edit = models.StoryQueue.update(**args).where(models.StoryQueue.id == id)
+    edit.execute()
+    #.update only returns the num of rows changed. so, if you want it
+    # to return the updated db entry, requery:
+    changed_storyqueue = models.StoryQueue.get(models.StoryQueue.id == id) #returns updated object
+    return changed_storyqueue
+
+  def delete(self, id):
+    query = models.StoryQueue.delete().where(models.StoryQueue.id == id)
+    query.execute()
+    return 'resource deleted'
+
+storyqueues_api = Blueprint('resources.storyqueues', __name__)
+api = Api(storyqueues_api)
+
+api.add_resource(
+  StoryQueueList,
+  '/storyqueues',
+  endpoint='storyqueues'
+)
+
+api.add_resource(
+  StoryQueue,
+  '/storyqueues/<int:id>',
+  endpoint='storyqueue'
+)
+
+
+
+
+
+
+
+
+
+
